8f4d58e0526f6eb0d7678b0386a900613d46d8b4
Miguel Angel <miguelangelpena.dev@gmail.com>
2020-11-09 03:38:46 -0400

    Creating the Sidebar

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React,{useState} from "react";
 import Navbar from "../Navbar";
+import Sidebar from "../Sidebar";
 import { HeroContainer, HeroContent,HeroItems,HeroH1,HeroP,HeroBtn } from './HeroElements'
 
 const Hero = () => {
+
+    //STATES
+    //state related with the menu, open or closed
+    const [isOpen,setIsOpen] = useState(false);
+
+    //HANDLERS
+    //handler related with the menu, open or close is called
+    const handleToggle = ()=>{
+        setIsOpen(!isOpen);
+    }
+
   return (
     <HeroContainer>
-      <Navbar />
+      <Navbar toggle={handleToggle}/>
+      <Sidebar isOpen={isOpen} toggle={handleToggle}/>
       <HeroContent>
           <HeroItems>
               <HeroH1>Greatest Pizza Ever</HeroH1>
